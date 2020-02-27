6915a469f0e3b22e98b9cef7a6a5373ed8c63eb9
Tracy Collins <tc@threeceemedia.com>
2020-02-27 07:36:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,5 +61,6 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined){
     return ("Sorry, we don\'t have a credit card on file for you.")
   }
+  return(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   // write your code here
 }
