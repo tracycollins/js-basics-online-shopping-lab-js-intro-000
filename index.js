e03032ebac91c34ab3b9c29234cbeafc6c9c4bb7
Tracy Collins <tc@threeceemedia.com>
2020-02-27 07:23:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,7 +36,11 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  var totalCost = 0;
+  for (var i=0; i<cart.length; i++){
+    totalCost += cart[i].itemPrice;
+  }
+  return totalCost
 }
 
 function removeFromCart(item) {
